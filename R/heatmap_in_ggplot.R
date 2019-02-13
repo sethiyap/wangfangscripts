62df36fa3ec3cd807c4018af6695b1ad47bafa67
sethiyap <poojasethiya24@gmail.com>
2019-02-13 14:10:15 +0800

    heatmap sort high to low short functions added

--- R/heatmap_in_ggplot.R
+++ R/heatmap_in_ggplot.R
@@ -1,72 +1,77 @@
 heatmap_in_ggplot <-
 function(dat, output_name){
-          
+
           #--- Load libraries
           library(ggplot2)
           library(tidyverse)
-          
+
           #--- Data format
           # Gene	Spore	Hypha	Input	Category
           # cetL	111.7443	2.8244	5.3759	Conidia enriched transcript
           # wetA	42.2475	3.1493	6.7317	Spore maturation
           # wA	36.6445	2.2049	4.3408	Spore maturation
-          
+
           #--- facet if different Categories provided
           if ("Category" %in% colnames(dat)){
-                    
+
                     message("plotting categories...")
                     #--- Gather data for plotting
-                    dat_plot <-   dat %>% gather(key="Sample", value="fpkm",-Gene, -Category) %>% mutate(Sample=as_factor(Sample), Gene=as_factor(Gene))
-                    
+                    dat_plot <-   dat %>% gather(key="Sample", value="fpkm",-Gene, -Category) %>%
+                              mutate(Sample=as_factor(Sample), Category=as_factor(Category)) %>% group_by(Category,Sample) %>%
+                              arrange((fpkm),.by_group = TRUE)
+
+                    dat_plot$Gene <- factor(dat_plot$Gene, levels=unique(dat_plot$Gene))
                     #--- Plot heatmap
                    gg= ggplot(data = dat_plot, aes(x = Sample, y = Gene)) +
-                              geom_tile(aes(fill = log2(fpkm)), colour = "white") + 
-                              facet_wrap(~Category, nrow=3,scales = "free_y", strip.position="top") +
+                              geom_tile(aes(fill = log2(fpkm)), colour = "white") +
+                              facet_wrap(~Category, nrow=length(unique(dat_plot$Category)),scales = "free_y", strip.position="left") +
                               scale_fill_gradient(low = "#ffeda0",high = "#f03b20", limits = c(0,max(log2(dat_plot$fpkm)))) +theme_bw()+
                               scale_y_discrete(position="right")+
-                              theme(axis.text.x= element_text(face="bold", colour="black", size=12,angle=0,vjust=0.8),
-                                    axis.text.y = element_text(face="bold", color="black",size=12),
+                              theme(axis.text.x= element_text(face="bold", colour="black", size=10,angle=0,vjust=0.8),
+                                    axis.text.y = element_text(face="bold", color="black",size=10),
                                     axis.title.y=element_blank(),
                                     axis.title.x=element_blank(),
                                     legend.position = "top",
                                     #strip.background = element_rect( fill="white"),
                                     strip.text=element_text(face="bold", color="black",size=11),
-                                    legend.title=element_text(face="bold", color="black",size=12),
+                                    legend.title=element_text(face="bold", color="black",size=10),
                                     legend.key.size = unit(1,"line"),
                                     legend.box.spacing=unit(0.5,"mm"),
-                                    legend.text=element_text(face="bold", color="black",size=12))+
+                                    legend.text=element_text(face="bold", color="black",size=10))+
                               guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5,title ="log2(RNA Pol-II Signal)"))
-                   
+
                    print(gg)
-                   ggsave(filename = paste(output_name,"_heatmap_in_ggplot.png"), device = "png", path="./", width = 8, height =9,unit="cm", dpi=300)
-                    
+                   ggsave(filename = paste(output_name,"_heatmap_in_ggplot.png"), device = "png", path="./", width = 8, height =19,unit="cm", dpi=300)
+
           }
-          
+
           else{
                     message("no category provided! plotting data...")
-                    dat_plot <-   dat %>% gather(key="Sample", value="fpkm",-Gene) %>% mutate(Sample=as_factor(Sample), Gene=as_factor(Gene))
-                    
+                    dat_plot <-   dat %>% gather(key="Sample", value="fpkm",-Gene) %>%
+                                        mutate(Sample=as_factor(Sample)) %>% group_by(Sample) %>% arrange((fpkm),.by_group=TRUE) # Order high to low
+
+                    dat_plot$Gene <- factor(dat_plot$Gene, levels=unique(dat_plot$Gene))
                   gg=ggplot(data = dat_plot, aes(x = Sample, y = Gene)) +
-                              geom_tile(aes(fill = log2(fpkm)), colour = "white") + 
+                              geom_tile(aes(fill = log2(fpkm)), colour = "white") +
                               scale_fill_gradient(low = "#ffeda0",high = "#f03b20",limits = c(0,max(log2(dat_plot$fpkm)))) +theme_bw()+
                               scale_y_discrete(position="right")+
-                              theme(axis.text.x= element_text(face="bold", colour="black", size=12,angle=0,vjust=0.8),
-                                    axis.text.y = element_text(face="bold", color="black",size=12),
+                              theme(axis.text.x= element_text(face="bold", colour="black", size=10,angle=0,vjust=0.8),
+                                    axis.text.y = element_text(face="bold", color="black",size=10),
                                     axis.title.y=element_blank(),
                                     axis.title.x=element_blank(),
                                     legend.position = "top",
-                                    legend.title=element_text(face="bold", color="black",size=12),
+                                    legend.title=element_text(face="bold", color="black",size=10),
                                     legend.key.size = unit(1,"line"),
                                     legend.box.spacing=unit(0.5,"mm"),
-                                    legend.text=element_text(face="bold", color="black",size=12))+
+                                    legend.text=element_text(face="bold", color="black",size=10))+
                               guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5,title ="log2(RNA Pol-II Signal)"))
-                    
+
                      print(gg)
-                     
+
                      ggsave(filename = paste(output_name,"_heatmap_in_ggplot.png"), device = "png", path="./", width = 8, height =8,unit="cm", dpi=300)
-                     
-          }        
-          
-          
-          
+
+          }
+
+
+
 }
---
+++ R/miscellaneous_function.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+
+#supp correlation ----
+dat_scatter <- read_delim(pipe("pbpaste"), delim="\t", col_names = TRUE)
+
+# Gene     Set1  Set2
+# AN7895   3.43  4.88
+# AN8366   2.43  4.60
+# AN0833   8.74  9.27
+# AN11702  3.07  6.40
+
+plot_correlation_by_scatter <- function(dat_scatter, output_name, plot_title="Sample", color="black"){
+
+          if ("Category" %in% colnames(dat_scatter)){
+                        gg <- ggplot(dat_scatter,aes(log2(Set1), log2(Set2)))+geom_point(alpha=0.5, color=color)+
+                                        facet_wrap(~Class)+
+                                        labs(x="Set1", y="Set2", title=plot_title)+
+                                        theme_bw()+
+                                        theme(axis.text.x= element_text(face="bold", color="black",size=10),
+                                        axis.text.y = element_text(face="bold", color="black",size=10),
+                                        axis.title.y=element_text(face="bold", color="black",size=10),
+                                        axis.title.x=element_text(face="bold", color="black",size=10),
+                                        legend.title=element_text(face="bold", color="black",size=10),
+                                        strip.text = element_text(face="bold", color="black",size=10),
+                                        legend.key.size = unit(1,"line"),
+                                        plot.title = element_text(hjust = 0.5),
+                                        strip.background = element_rect(colour = "black", fill = "white"),
+                                        legend.text=element_text(face="bold", color="black",size=14))+
+                                        guides(fill = guide_legend(title = ""), color=guide_legend(title=""))
+                        print(gg)
+                    ggsave(filename = paste(output_name,"_correlationByScatter.png"), device = "png", path="./", width = 8, height =8,unit="cm", dpi=300)
+
+          }
+
+          else{
+                    gg <- ggplot(dat_scatter,aes(log2(Set1), log2(Set2)))+geom_point(alpha=0.5, color=color)+
+                              theme_bw()+labs(x="Set1", y="Set2", title=plot_title)+
+                              theme(axis.text.x= element_text(face="bold", color="black",size=10),
+                                    #axis.ticks = element_blank(),
+                                    axis.text.y = element_text(face="bold", color="black",size=10),
+                                    axis.title.y=element_text(face="bold", color="black",size=10),
+                                    axis.title.x=element_text(face="bold", color="black",size=10),
+                                    legend.title=element_text(face="bold", color="black",size=10),
+                                    strip.text = element_text(face="bold", color="black",size=10),
+                                    legend.key.size = unit(1.5,"line"),
+                                    plot.title = element_text(hjust = 0.5),
+                                    strip.background = element_rect(colour = "black", fill = "white"),
+                                    legend.text=element_text(face="bold", color="black",size=14))+
+                              guides(fill = guide_legend(title = ""), color=guide_legend(title=""))
+                    print(gg)
+
+                    ggsave(filename = paste(output_name,"_correlationByScatter.png"), device = "png", path="./", width = 8, height =8,unit="cm", dpi=300)
+
+          }
+}
+
