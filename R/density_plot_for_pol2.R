b70cdd44ced3c3f8737827779af1c8b1ea15f4d0
sethiyap <poojasethiya24@gmail.com>
2019-02-21 14:20:23 +0800

    first commit

---
+++ R/density_plot_for_pol2.R
@@ -0,0 +1,54 @@
+Pol2_dat <- read_delim(pipe("pbpaste"),delim="\t", col_names = TRUE)
+
+#' Title density_plot_for_pol2
+#'
+#' @param Pol2_dat add Pol2 data in tabular format with Gene, sample_1, sample_2
+#' @param outfile pdf of the desnity plot
+#' @param background_value determine the background to highlight the background value
+#'
+#' @return
+#' @export
+#' @import tidyverse
+#' @import extrafont
+#' @author pooja sethiya
+#' @examples
+#'
+density_plot_for_pol2 <- function(Pol2_dat, outfile, background_value){
+
+          library(tidyverse)
+          library(extrafont)
+          loadfonts(device = "pdf")
+
+          #--- rename the coumns
+          colnames(Pol2_dat) <- c("Gene","RNA Pol II Signal", "Input")
+
+          #--- reshape data for plotting
+          ppm <- Pol2_dat %>% gather(key=variable, value=value,-Gene)
+
+          #--- plot ggplot
+          gg = ggplot(ppm,aes(value+0.01,fill=variable))+geom_density(alpha=0.5)+xlab("Pol2 Occupancy Value")+theme_bw()+
+                    scale_fill_manual(values=c("#ffeda0","#f03b20"))+
+                    geom_vline(xintercept=background_value,size = 1, colour = c("grey"),linetype = "dashed",show.legend = TRUE)+
+                    scale_x_sqrt()+
+                    theme(axis.text.x= element_text( colour="black", size=12,angle=0,vjust=0.8,family="Arial"),
+                          axis.text.y = element_text( color="black",size=12,family="Arial"),
+                          axis.title.y=element_text(color="black",size=12,family="Arial"),
+                          axis.title.x=element_text(color="black",size=12,family="Arial"),
+                          legend.title=element_blank(),
+                          legend.key.size = unit(1,"line"),
+                          legend.position = c(.80, .85),
+                          legend.text=element_text( color="black",size=12,family="Arial"))+
+                    geom_text(aes(x=background_value,label=paste("\n background =", round(background_value,2))), y=0.4,angle=90,colour="black", size=6,family="Arial")+
+                    guides(fill = guide_legend(title=""))+labs(x="RNA Pol II occupancy", y="Density")
+
+          print(gg)
+
+          #--- save file
+          ggsave(filename = paste(outfile,"_Pol2_distribution.pdf",sep=""), path="./",width=5, height=5, dpi=300)
+}
+
+
+
+
+
+
